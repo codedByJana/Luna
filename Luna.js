dd85e9ca86edc71eaafca21bad4a3e6029e04405
codedByJana <2401929@student.eelu.edu.eg>
2026-08-29 06:26:40 +0300

    fix console error

--- Luna.js
+++ Luna.js
@@ -38,7 +38,7 @@ for (const file of commandFiles) {
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command);
   } else {
-    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    console.log(`[WARNING] The command at ${file} is missing a required "data" or "execute" property.`);
   }
 }
 process.on('unhandledRejection', (reason, promise) => {
